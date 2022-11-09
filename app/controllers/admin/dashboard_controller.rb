f4d17aefa3fb1b02cb881ee00334b626b9fadc26
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-11-09 00:45:22 +0000

    contain a dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @count_of_product = Product.all.count
+    @count_of_evergreens = Product.where(category_id: 1).count
+    @count_of_shrubs = Product.where(category_id:2).count
+    @count_of_trees = Product.where(category_id:3).count
   end
 end
