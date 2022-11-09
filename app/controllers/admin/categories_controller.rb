e0ae35a7fc5d9c65621169c534d3521efae57509
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-11-09 20:56:13 +0000

    contain all the functionality to make categories work! still need to refactor and style

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,40 +2,42 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
 
   def index
-    @products = Product.order(id: :desc).all
+      @category_name_to_id = {}
+      @product_per_category_count = {}
+      @categories = Category.all
+      @product_category = Category.joins(:products)
+
+      @categories.each do |c|
+        @product_per_category_count[c.name] = 0
+        @category_name_to_id[c.name] = c.id
   end
 
-  def new
-    @product = Product.new
+  @product_category.map do |n|
+    @product_per_category_count[n.name] += 1
   end
+end
 
-  def create
-    @product = Product.new(product_params)
+def new
+  @category = Category.new
+end
 
-    if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
-    else
-      render :new
-    end
+def create
+  @category = Category.new(category_params)
+
+  if @category.save
+    redirect_to [:admin, :categories], notice: 'Product created!'
+  else
+    render :new
   end
+end
 
-  def destroy
-    @product = Product.find params[:id]
-    @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
-  end
+private
 
-  private
+def category_params
+  params.require(:category).permit(
+    :name,
+  )
 
-  def product_params
-    params.require(:product).permit(
-      :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
-    )
   end
 
 end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,7 @@ class Admin::DashboardController < ApplicationController
     @count_of_evergreens = Product.where(category_id: 1).count
     @count_of_shrubs = Product.where(category_id:2).count
     @count_of_trees = Product.where(category_id:3).count
+    @category = Category.all
+    @product = Product.all
   end
 end
