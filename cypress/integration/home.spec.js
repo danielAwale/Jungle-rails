a4326134c1603a8a83cbbf86c0216451c38cbd3b
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-11-13 06:13:09 +0000

    pass our test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,4 +2,8 @@ describe('example to-do app', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
 })
